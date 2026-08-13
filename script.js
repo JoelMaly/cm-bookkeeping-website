5cc17dff06eaf082d8e40ce78bac4438ca6ce775
JoelMaly <joel.maly@gmail.com>
2026-08-13 12:20:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// CM Bookkeeping Solutions
+// Website JavaScript
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("CM Bookkeeping Solutions website loaded.");
+
+});
